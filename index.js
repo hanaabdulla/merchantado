6885001c37470e3cba651c5b1c7fbda0e3e2932b
hanaabdulla <hanaabdulla@ug.cusat.ac.in>
2023-05-30 14:16:25 +0530

    landing page

---
+++ index.js
@@ -0,0 +1,10 @@
+var navLink = document.getElementById('navlinks')
+
+function showmenu(){
+     navLink.style.right = "0";
+     
+}
+function hidemenu(){
+     navLink.style.right = "-200px";
+
+}
